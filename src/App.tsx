5cc0aeda195a2fe23f6794ce7c7d1fa63075d476
Dasmini <dasmini2001@gmail.com>
2023-11-01 11:31:17 +0530

    my commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <ul>
+        <li>Dasmini</li>
+        <li>Princes de France</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
